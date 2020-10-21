f036b42838396474e24cc8c47c9e1685877d4d37
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-21 15:04:05 +0530

    :sparkles: Add v1

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,12 @@
-test("hello world", () => {
-  expect("world").toBe("world");
+import { Unqueue } from "./";
+test("create object", () => {
+  const queue = new Unqueue();
+  expect(queue).toBeDefined();
+});
+
+test("add a task", () => {
+  const queue = new Unqueue();
+  expect(queue.getPendingTasks().length).toBe(0);
+  queue.add(async () => {});
+  expect(queue.running).toBeTruthy();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-export const hello = "world";
+export interface UnqueueConfig {
+  /** Maximum number of attempts to try, defaults to 3 */
+  maxAttempts?: number;
+  /** Debug logging */
+  debug?: boolean;
+  /** Interval to schedule */
+  ttl?: number;
+}
+
+/** Unqueue is a queue for async JS */
+export class Unqueue {
+  tasks: Array<{ task: () => Promise<any>; attempt: number }> = [];
+  running = false;
+  config: UnqueueConfig = {};
+
+  constructor(config?: UnqueueConfig) {
+    if (config) this.config = config;
+    setInterval(() => this.run(), this.config.ttl ?? 3600);
+  }
+
+  private log(...args: any[]) {
+    if (this.config.debug) console.log(...args);
+  }
+
+  private async run() {
+    const maxAttempts = this.config.maxAttempts ?? 3;
+    this.running = true;
+    const task = this.tasks.pop();
+    if (task) {
+      this.log("Starting task", task.task.name);
+      if (task.attempt <= maxAttempts) {
+        try {
+          await task.task();
+        } catch (error) {
+          this.log("Got an error", error);
+          this.add(task.task, task.attempt + 1);
+        }
+      } else {
+        this.log("More than 3 attempts, skipping");
+      }
+    } else {
+      this.log("No task");
+    }
+    if (this.tasks.length) await this.run();
+    this.running = false;
+  }
+
+  /** Add a task to the queue */
+  public add(task: () => Promise<any>, attempt = 1) {
+    this.tasks.push({ task, attempt });
+    if (!this.running) this.run();
+  }
+
+  /** Get pending tasks */
+  public getPendingTasks() {
+    return this.tasks;
+  }
+}
