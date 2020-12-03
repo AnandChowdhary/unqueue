be0be2a28a86740c10d7604ab24627d43bb06847
Anand Chowdhary <mail@anandchowdhary.com>
2020-12-03 15:45:24 +0530

    :poop: Remove pending tasks tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,10 +3,3 @@ test("create object", () => {
   const queue = new Unqueue();
   expect(queue).toBeDefined();
 });
-
-test("add a task", () => {
-  const queue = new Unqueue();
-  expect(queue.getPendingTasks().length).toBe(0);
-  queue.add(async () => {});
-  expect(queue.running).toBeTruthy();
-});
